0b4846f6a3917165d2a860ae04c02cb66c10e1e5
AbdouVak <abdou.vakouiev@gmail.com>
2023-10-30 16:51:46 +0100

    Création du dashboardController

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Exercice;
+use App\Entity\CategorieMuscle;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Projet Sport');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Muscle', 'fas fa-list', CategorieMuscle::class);
+        yield MenuItem::linkToCrud('Exercice', 'fas fa-list', Exercice::class);
+    }
+}
