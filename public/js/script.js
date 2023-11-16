40ff4cdbc2fdae4114f25dca44aa08ebd49fa384
AbdouVak <abdou.vakouiev@gmail.com>
2023-11-16 11:52:42 +0100

    changement dans le css

--- public/js/script.js
+++ public/js/script.js
@@ -34,4 +34,32 @@ document.addEventListener("DOMContentLoaded", function() {
     topicForm.submit();
   });
 
+  // Get all seance cards
+  var seanceCards = document.querySelectorAll('.cardSeance');
+
+  // Add click event listener to each seance card
+  seanceCards.forEach(function (card) {
+    card.addEventListener('click', function () {
+      // Get the corresponding seance content
+      var seanceContent = card.querySelector('.seanceContent').innerHTML;
+
+      // Show the modal with the seance content
+      showModal(seanceContent);
+    });
+  });
+
+  // Function to show the modal with content
+  function showModal(content) {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the modal content element
+    var modalContent = modal.querySelector(".modal-content");
+
+    // Set the content of the modal
+    modalContent.innerHTML = content;
+
+    // Display the modal
+    modal.style.display = "block";
+  }
 });
