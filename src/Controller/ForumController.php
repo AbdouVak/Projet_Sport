e4d31e8b525f43a9276f3a16dc29622a5cc1bb74
AbdouVak <abdou.vakouiev@gmail.com>
2023-11-20 16:57:01 +0100

    ForumController: controller qui remplce ce que le topic controller faisait

---
+++ src/Controller/ForumController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\Topic;
+use App\Form\TopicType;
+use App\Form\ForumType;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\TopicRepository;
+use App\Repository\CategorieTopicRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ForumController extends AbstractController
+{
+    #[Route('/forum', name: 'app_forum')]
+    public function index(Topic $topic = null,Request $request,TopicRepository $topicRepository,CategorieTopicRepository $categorieTopicRepository,ManagerRegistry $doctrine): Response
+    {   
+        if(!$topic){
+            $topic = new Topic();
+        }
+
+        $topics = $topicRepository->findAll();
+
+        /* Trie les topic par categorie */
+        if (!empty($_POST['categorieTopic'])){
+            $topics = $topicRepository->findTopicsByCategory($_POST['categorieTopic']);
+        }else{
+            $topics = $topicRepository->findAll();
+        }
+        $categorieTopics = $categorieTopicRepository->findAll();
+        
+        /* Affiche les topic par rapport au recherche */
+        $formSearchTopic = $this->createForm(ForumType::class);
+        $formSearchTopic->handleRequest($request);
+
+        if ($formSearchTopic->isSubmitted() && $formSearchTopic->isValid()) {
+            $searchTerm= $formSearchTopic->get('search')->getData();
+
+            $topics = $topicRepository->findTopicsBySearch($searchTerm);
+        }
+
+        /* Ajouter un topic */
+        $formAddTopic = $this->createForm(TopicType::class);
+        $formAddTopic->handleRequest($request);
+
+        if ($formAddTopic->isSubmitted() && $formAddTopic->isValid()) {
+            $topic = $formAddTopic->getData();
+            $topic->setUser($this->getUser());
+            $topic->setDateCreation(new DateTime());
+            $topic->setVerrouiller(false);
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($topic);
+            $entityManager->flush();
+        }
+
+        return $this->render('forum/index.html.twig', [
+            'categorieTopics' => $categorieTopics,
+            'formSearchTopic' => $formSearchTopic->createView(),
+            'formAddTopic' => $formAddTopic->createView(),
+            'topics' => $topics
+        ]);
+    }
+}
