60be00718bfd78f7580d5c55235d9c85c17fb1cc
AbdouVak <abdou.vakouiev@gmail.com>
2023-12-07 16:55:36 +0100

    JS: ajout pour le menu burger

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,25 @@
 function toggleMenu() {
-  var navbarLinks = document.querySelector('.navbar-links');
-  if (navbarLinks.style.display === 'flex') {
-      navbarLinks.style.display = 'none';
-  } else {
-      navbarLinks.style.display = 'flex';
-  }
+    var navbarList = document.querySelector('.navbar-list');
+    navbarList.classList.toggle('show');
 }
+
+
 $(document).ready(function () {
   // Lorsque le lien est cliqué
-  $("#ajouterSeance").click(function () {
+    $("#ajouterSeance").click(function () {
       // Afficher la modal
-      $("#modalAddSeance").show();
-  });
+    $("#modalAddSeance").show();
+    });
 
   // Ajouter un écouteur d'événements pour les clics à l'extérieur de la modal
-  $(document).on('mouseup', function (e) {
-      var modal = $("#modalAddSeance");
+    $(document).on('mouseup', function (e) {
+        var modal = $("#modalAddSeance");
 
       // Vérifier si l'élément cliqué n'est pas à l'intérieur de la modal
-      if (!modal.is(e.target) && modal.has(e.target).length === 0) {
+        if (!modal.is(e.target) && modal.has(e.target).length === 0) {
           // Cacher la modal
-          modal.hide();
-      }
-  });
+            modal.hide();
+    }
+});
 });
 
