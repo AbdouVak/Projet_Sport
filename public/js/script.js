87d0e52de64b941443be5143f8c7a886bca9629f
AbdouVak <abdou.vakouiev@gmail.com>
2023-12-08 16:52:58 +0100

    JS:ajout du js lorsqu'on clique sur uen seance on affiche leur exercice

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,50 @@
+/* ------------------ MenuBurger ------------------*/
 function toggleMenu() {
     var navbarList = document.querySelector('.navbar-list');
     navbarList.classList.toggle('show');
 }
 
 
-$(document).ready(function () {
-  // Lorsque le lien est cliqué
-    $("#ajouterSeance").click(function () {
-      // Afficher la modal
-    $("#modalAddSeance").show();
-    });
+/* ------------------ Seance ------------------*/
 
-  // Ajouter un écouteur d'événements pour les clics à l'extérieur de la modal
-    $(document).on('mouseup', function (e) {
-        var modal = $("#modalAddSeance");
+var seanceContainer = document.querySelector('.seanceContainer');
 
-      // Vérifier si l'élément cliqué n'est pas à l'intérieur de la modal
-        if (!modal.is(e.target) && modal.has(e.target).length === 0) {
-          // Cacher la modal
-            modal.hide();
-    }
+  // Ajoutez un écouteur d'événements pour le clic
+seanceContainer.addEventListener('click', function() {
+  toggleDivs();
 });
-});
+
+function toggleDivs() {
+  var seanceInfo = document.querySelector('.seanceInfo');
+  var seanceDesc = document.querySelector('.seanceDesc');
+
+  // Vérifie si div1 est actuellement visible
+  if (seanceInfo.style.display !== 'none') {
+    seanceInfo.style.display = 'none';
+    seanceDesc.style.display = 'block'; // ou 'inline' ou 'inline-block' selon vos besoins
+  } else {
+    seanceInfo.style.display = 'block'; // ou 'inline' ou 'inline-block'
+    seanceDesc.style.display = 'none';
+  }
+}
+
+/* ------------------ Modal ------------------*/
+// $(document).ready(function () {
+//   // Lorsque le lien est cliqué
+//     $("#ajouterSeance").click(function () {
+//       // Afficher la modal
+//     $("#modalAddSeance").show();
+//     });
+
+//   // Ajouter un écouteur d'événements pour les clics à l'extérieur de la modal
+//     $(document).on('mouseup', function (e) {
+//         var modal = $("#modalAddSeance");
+
+//       // Vérifier si l'élément cliqué n'est pas à l'intérieur de la modal
+//         if (!modal.is(e.target) && modal.has(e.target).length === 0) {
+//           // Cacher la modal
+//             modal.hide();
+//     }
+// });
+// });
 
